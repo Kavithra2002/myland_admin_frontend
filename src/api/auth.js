4ec007e9cdc9e5663181910307c26cf08655edf7
Kavithra2002 <kmethnula@gmail.com>
2026-09-17 08:31:54 +0530

    Remove unused mail-test API helpers

--- src/api/auth.js
+++ src/api/auth.js
@@ -8,19 +8,6 @@ export async function login(email, password) {
   return readError(res, 'Could not sign in');
 }
 
-export async function fetchMailStatus() {
-  const res = await apiFetch('/api/mail/status');
-  const data = await readError(res, 'Could not check mail status');
-  return data;
-}
-
-export async function sendTestMailRequest() {
-  const res = await apiFetch('/api/mail/test', { method: 'POST' });
-  const data = await res.json().catch(() => ({}));
-  if (!res.ok) throw new Error(data.reason || data.message || 'Could not send test mail');
-  return data;
-}
-
 export async function fetchMe() {
   const res = await apiFetch('/api/auth/me');
   const data = await readError(res, 'Could not load session');
